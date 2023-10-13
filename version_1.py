afc3e408490d55c847de36e7643502bd4555b8ed
Victor Cancino <11082428+Ninigocancino@users.noreply.github.com>
2023-10-12 20:13:13 -0600

    Agregamos la version 1 del proyecto

---
+++ version_1.py
@@ -0,0 +1,11 @@
+#Crear un programa que convierta el peso de un objeto (en kg) a su equivalente en el planeta Marte y lo devuelva en pantalla
+
+#Peso en marte = peso en la tierra * gravedad en marte / gravedad en la tierra
+
+gravedad_marte = 0.38
+gravedad_tierra = 1
+peso_en_la_tierra = 35
+
+peso_marte = peso_en_la_tierra * gravedad_marte / gravedad_tierra
+
+print(peso_marte)
