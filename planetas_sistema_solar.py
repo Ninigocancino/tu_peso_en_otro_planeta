a5b7b6054c5e8802783af84dc4b129f44c238e85
Victor Cancino <11082428+Ninigocancino@users.noreply.github.com>
2023-11-15 19:53:33 -0600

    The file 'planetas_sistema_solar.py' was added to the directory, and the list of dictionaries 'planetas_ss' was created

---
+++ planetas_sistema_solar.py
@@ -0,0 +1,56 @@
+planetas_ss = [
+    {
+        "nombre": "Mercurio",
+        "dimension": "4,879 km (diámetro ecuatorial)",
+        "gravedad": "3.7 m/s²",
+        "gravedad_relativa": 0.38  # En comparación con la Tierra
+    },
+    {
+        "nombre": "Venus",
+        "dimension": "12,104 km (diámetro ecuatorial)",
+        "gravedad": "8.87 m/s²",
+        "gravedad_relativa": 0.91
+    },
+    {
+        "nombre": "Tierra",
+        "dimension": "12,742 km (diámetro ecuatorial)",
+        "gravedad": "9.81 m/s²",
+        "gravedad_relativa": 1.00
+    },
+    {
+        "nombre": "Marte",
+        "dimension": "6,779 km (diámetro ecuatorial)",
+        "gravedad": "3.71 m/s²",
+        "gravedad_relativa": 0.38
+    },
+    {
+        "nombre": "Jupiter",
+        "dimension": "139,820 km (diámetro ecuatorial)",
+        "gravedad": "24.79 m/s²",
+        "gravedad_relativa": 2.53
+    },
+    {
+        "nombre": "Saturno",
+        "dimension": "116,460 km (diámetro ecuatorial)",
+        "gravedad": "10.44 m/s²",
+        "gravedad_relativa": 1.06
+    },
+    {
+        "nombre": "Urano",
+        "dimension": "50,724 km (diámetro ecuatorial)",
+        "gravedad": "8.69 m/s²",
+        "gravedad_relativa": 0.88
+    },
+    {
+        "nombre": "Neptuno",
+        "dimension": "49,244 km (diámetro ecuatorial)",
+        "gravedad": "11.15 m/s²",
+        "gravedad_relativa": 1.14
+    },
+    {
+        "nombre": "Pluton",
+        "dimension": "2,377 km (diámetro ecuatorial)",
+        "gravedad": "0.62 m/s²",
+        "gravedad_relativa": 0.06
+    }
+]
